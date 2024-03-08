2d595134c7398976cca376a0767797f18b61f22d
haime62 <haime62@gmail.com>
2024-03-08 18:08:36 +0200

    Development from cottage laptop

--- htmlnode.py
+++ htmlnode.py
@@ -4,11 +4,9 @@ class HTMLNode:
         self.value = value
         self.children = children or []
         self.props = props or {}
-        print(props)
 
     def to_html(self):
         raise NotImplemented
-
     def props_to_html(self):
         return "".join(map(lambda x: f' {x[0]}="{x[1]}"', self.props.items()))
 
@@ -18,4 +16,54 @@ class HTMLNode:
                 f"children={", ".join(self.children)}, "
                 f"props={", ".join(map(lambda x: f'{x[0]}:{x[1]}', self.props.items()))}")
 
+    def tagger(self):
+        match self.tag:
+            case "a": #hyperlink
+                return f"<a{self.props_to_html()}>{self.value}</a>"
+            case "p": #paragraph
+                return f"<p>{self.value}</p>"
+            case "b": #bold
+                return f"<b>{self.value}</b>"
+            case "i": #italic
+                return f"<i>{self.value}</i>"
 
+            case _:
+                raise ValueError("Tag not recognized")
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, *children, **props):
+        if children:
+            raise ValueError("Leaf can't have children")
+        if value is None:
+            raise ValueError("Must have value")
+        super().__init__(tag=tag, value=value, *children, **props)
+        if self.value is None:
+            raise ValueError("Must have value")
+
+
+    def to_html(self):
+        if self.tag is None:
+            return self.value
+        return self.tagger()
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, value=None, *children, **props):
+        if tag is None:
+            raise ValueError("Parent must have tag")
+        if not children:
+            raise ValueError("Parent must have children")
+        super().__init__(tag=tag, value=value, *children, **props)
+
+    def to_html(self):
+        return "".join(map(lambda x: x.to_html, self.children))
+
+
+node = ParentNode(
+    "p", None, *[LeafNode("b", "Bold text"), LeafNode(None, "Normal text"), LeafNode("i", "italic text"), LeafNode(None, "Normal text")],
+)
+
+
+#node = LeafNode("p","toimiiko")
+print(node.to_html())
--- test_htmlnode.py
+++ test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 tt = {"href": "https://www.google.com", "target": "_blank"}
 
@@ -7,6 +7,10 @@ class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode(**tt)
         self.assertEqual(' href="https://www.google.com" target="_blank"', node.props_to_html())
+
+    def test_leafnode(self):
+        node = LeafNode(value="toimii", tag="p")
+
 
 
 if __name__ == '__main__':
